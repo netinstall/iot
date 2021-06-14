8d41ec2888db35f17570bbd4eea7623838cb3782
Petr Burmistrov <Petr Burmistrov>
2021-06-15 00:42:29 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
-setup(
-    name='django-static-sites',
-    version='0.0.8',
-    packages=find_packages("src"),  # include all packages under src
-    package_dir={"": "src"},   # tell distutils packages are under src
 
-    package_data={
-        "": ["*.txt"],
-        "mypkg": ["data/*.dat"],
-    }
-)
+
+setup(
+    name="iot",
+    version="0.1.2",
+    packages=find_packages("web"),
+    package_dir={"": "web"},
+    scripts=["web/web.py"],
+    description="IOT",
+    install_requires=[
+        "bottle",
+        "requests"],
+    package_data={"": ["web/templates/*"]}
+) 
