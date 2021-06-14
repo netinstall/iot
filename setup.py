f2b201734f3e0d0c748b1e19d682364be489d38f
Petr Burmistrov <Petr Burmistrov>
2021-06-15 00:39:50 +0300

    fix

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-static-sites',
     version='0.0.8',
-    package_dir={"": "src"},   # tell distutils packages are under src
-
+    packages=find_packages("src"),  # include all packages under src
     package_data={
         "": ["*.txt"],
         "mypkg": ["data/*.dat"],
