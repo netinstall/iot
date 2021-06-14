68b54b43924e842905036b2adf7d4b17787d62ad
Petr Burmistrov <Petr Burmistrov>
2021-06-15 01:00:38 +0300

    fix

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ from setuptools import setup, find_packages
 setup(
     name="iot",
     version="0.1.2",
+    description="IOT",
     packages=find_packages("."),
     package_dir={"": "."},
-    package_data={"web": ["templates/*"]},
-    description="IOT",
+    package_data={
+        "web": ["templates/*", "static/*",]
+    },
     install_requires=[
         "bottle",
         "requests"]
-) 
+)
