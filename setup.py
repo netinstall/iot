6d27699d81153d8399e61a05b5e3dceeab301859
Petr Burmistrov <Petr Burmistrov>
2021-06-14 03:17:03 +0300

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='iot',
     version='0.0.9',
     packages=find_packages(),
-    package_data={'web': ['static']},
+    package_data={'web': ['web/templates/main.tpl']},
     include_package_data=True,
     license='GNU GENERAL PUBLIC LICENSE',
     description='An easy to use Django app to make static site.',
