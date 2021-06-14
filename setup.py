75007a7da0324a4fc40d7bf6ade2606f5d2eb6e2
Petr Burmistrov <Petr Burmistrov>
2021-06-14 03:13:11 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,26 +1,10 @@
 from setuptools import setup, find_packages
 setup(
-    name='django-static-sites',
+    name='iot',
     version='0.0.8',
     packages=find_packages(),
-    package_data={'staticsites': ['templates/*/*-tpl', 'templates/*/*/*-tpl']},
+    package_data={'web': ['static']},
     include_package_data=True,
     license='GNU GENERAL PUBLIC LICENSE',
     description='An easy to use Django app to make static site.',
-    entry_points={
-        'console_scripts': ['django-static-admin=staticsites.management:execute_from_command_line'],
-    },
-    url='https://github.com/ciotto/django-static-sites',
-    author='Christian Bianciotto',
-    author_email='info@ci8.it',
-    package_dir={'django-static-sites': 'lib/django-static-sites'},
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-    ]
 )
