594cefcc0aeae420011d42755dd8f0d41014f17a
Petr Burmistrov <Petr Burmistrov>
2021-06-14 03:14:18 +0300

    ver++

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='iot',
-    version='0.0.8',
+    version='0.0.9',
     packages=find_packages(),
     package_data={'web': ['static']},
     include_package_data=True,
