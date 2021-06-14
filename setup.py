5db50b596e4d08235f0d1b953fd4cdd4afe82b03
Petr Burmistrov <Petr Burmistrov>
2021-06-15 00:44:51 +0300

    fix

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     version="0.1.2",
     packages=find_packages("web"),
     package_dir={"": "web"},
+    package_data={"": ["web/templates/*"]},
     scripts=["web/web.py"],
     description="IOT",
     install_requires=[
         "bottle",
-        "requests"],
-    package_data={"": ["web/templates/*"]}
+        "requests"]
 ) 
